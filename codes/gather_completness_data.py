a0694301ae6d4977e95a45926b44a695c57c198d
schwarzam <gustavo.b.schwarz@gmail.com>
2025-02-04 19:03:40 -0300

    check point

--- codes/gather_completness_data.py
+++ codes/gather_completness_data.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool, Manager
 from logpool import control  # Assuming logpool is compatible with multiprocessing
 
 # File directory and bands setup
-dual_cats = os.listdir("/storage/splus/Catalogues/iDR5/idr5/dual/")
+dual_cats = os.listdir("/storage/splus/Catalogues/iDR5/idr5/single/")
 bands = ["J0378", "J0395", "J0410", "J0430", "J0515", "J0660", "J0861", "i", "r", "u", "z", "g"]
 limits = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
@@ -18,29 +18,33 @@ def sum_to_final_results(args):
     tablename, num = args
     control.info(f"Processing file {num}: {tablename}")
 
-    # Read FITS file
-    tab = Table.read(f"/storage/splus/Catalogues/iDR5/idr5/dual/{tablename}")
+    try:
+        # Read FITS file
+        tab = Table.read(f"/storage/splus/Catalogues/iDR5/idr5/single/{tablename}")
 
-    local_results = {limit: {band: 0 for band in bands} for limit in limits}
+        local_results = {limit: {band: 0 for band in bands} for limit in limits}
 
-    # Summing logic
-    control.info("Summing")
-    for band in bands:
-        for limit in limits:
-            lower = limit - 0.5
-            upper = limit + 0.5
-            
-            length = len(
-                tab[(tab["r_petro"] > lower) & 
-                    (tab["r_petro"] <= upper) &
-                    (tab[f"{band}_petro"] != 99)]
-            )
-            local_results[limit][band] += length
-
-    # Updating shared dictionary
-    for limit in limits:
+        # Summing logic
+        control.info("Summing")
         for band in bands:
-            final_results[limit][band] += local_results[limit][band]
+            for limit in limits:
+                lower = limit - 0.5
+                upper = limit + 0.5
+                
+                length = len(
+                    tab[(tab["r_petro"] > lower) & 
+                        (tab["r_petro"] <= upper) &
+                        (tab[f"{band}_petro"] != 99)]
+                )
+                local_results[limit][band] += length
+
+        # Updating shared dictionary
+        for limit in limits:
+            for band in bands:
+                final_results[limit][band] += local_results[limit][band]
+                
+    except Exception as e:
+        control.critical(f"Error processing {e} {tablename}")
 
     control.info(f"Done processing {tablename}")
 
@@ -57,4 +61,4 @@ if __name__ == "__main__":
 
     # Create and save DataFrame
     df = pd.DataFrame(rows)
-    df.to_csv("../data/dual.csv", index=False)
+    df.to_csv("../data/single.csv", index=False)
